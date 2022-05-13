cb01ebc7231e398008dd321dc5077c61a993f495
kscode <kashinggo@gmail.com>
2022-05-13 17:07:55 +0800

    create SDP offer

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let localStream;
 let remoteStream;
+let peerConnection;
 
+const servers = {
+    iceServers:[
+        {
+            urls:['stun:stun1.l.google.com:19302','stun:stun2.l.google.com:19302']
+        }
+    ]
+}
 
 let init = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({
@@ -13,12 +21,28 @@ let init = async () => {
 };
 
 let createOffer = async () => {
-    peerConnection = new RTCPeerConnection()
+    peerConnection = new RTCPeerConnection(servers)
 
     remoteStream = new MediaStream()
     
   document.getElementById("user-2").srcObject = remoteStream;
+
+  localStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, localStream)
+  })
   
+  peerConnection.ontrack = (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+          remoteStream.addTrack()
+      })
+  }
+
+  peerConnection.onicecandidate = async (event) => {
+      if (event.candidate) {
+          console.log('New ICE candidate: ', event.candidate)
+
+      }
+  }
   let offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer)
   console.log('Offer: ', offer)
